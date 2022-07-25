9a9eb8f8f8560ffa3ce14f040c00587b530dd3cb
issdq <work.issdq@gmail.com>
2022-07-25 12:51:37 +0700

    Initial commit

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Entities;
+using MoviesAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("/api/genres")]
+    //[ApiController]
+    public class GenresController: ControllerBase
+    {
+        private readonly IRepository repository;
+
+        public GenresController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet] // api/genres
+        [HttpGet("list")] // api/genres/list
+        [HttpGet("/allgenres")] // allgenres
+        public List<Genre> Get()
+        {
+            return repository.GetAllGenres();
+        }
+
+        [HttpGet("{Id:int}/{param2=stu}")] // api/genres/example
+        public Genre Get(int Id, string param2)
+        {
+            var genre = repository.GetGenreById(Id);
+
+            if (genre == null)
+            {
+                //return NotFound();
+            }
+
+            return genre;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MoviesAPI
+ Building RESTful Web APIs with ASP.NET Core 3.1
