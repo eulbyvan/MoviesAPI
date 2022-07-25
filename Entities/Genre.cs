b76666de2d2bedf9026fa3580457557e2f441299
issdq <work.issdq@gmail.com>
2022-07-25 15:26:48 +0700

    added attribute validation

--- Entities/Genre.cs
+++ Entities/Genre.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoviesAPI.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace MoviesAPI.Entities
 {
-    public class Genre
+    public class Genre: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="The field with name {0} is required")]
         [StringLength(10)]
+        //[FirstLetterUppercase]
         public string Name { get; set; }
 
         [Range(18,120)]
@@ -19,5 +21,18 @@ namespace MoviesAPI.Entities
         public string CreditCard { get; set; }
         [Url]
         public string Url { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter should be an uppercase", new string[] { nameof(Name) });
+                }
+            }
+        }
     }
 }
